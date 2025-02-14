c1a6baaf6e487349344a415001b97acec9131778
Aldar Bazarov <a.bazarov.sas@gmail.com>
2025-02-14 16:37:43 +0300

    add prettier & react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { Button } from "./components/ui/button"
+import { Routes, Route } from 'react-router';
+import { AppLayout } from './components/app-layout/app-layout';
 
 function App() {
   return (
-    <>
-      <Button>Test</Button>
-    </>
-  )
+    <Routes>
+      <Route index element={<>Login</>} />
+
+      <Route element={<AppLayout />}>
+        <Route path="employees" element={<>employees</>} />
+        <Route path="goods" element={<>goods</>} />
+        <Route path="reports" element={<>reports</>} />
+        <Route path="sales" element={<>sales</>} />
+        <Route path="purchases" element={<>purchases</>} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
